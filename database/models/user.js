88181c69f0eaa52e36d9870ed19fb4c029f961a9
hammer <hammer92@hotmail.es>
2021-06-08 01:02:50 -0500

    se crea la migracion y modelos de usuario

---
+++ database/models/user.js
@@ -0,0 +1,32 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: {
+        msg: 'Este correo electrónico ya está en uso.',
+        fields: ['email']
+      }
+    },
+    password: {
+      type: DataTypes.STRING
+    }
+  }, {
+    sequelize,
+    modelName: 'User'
+  })
+  return User
+}
